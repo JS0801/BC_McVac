4f471f6c49cc2bfec4d0364b7506c1f07c75ddc0
JS0801 <jsuthar@oncloudconsulting.ca>
2026-04-16 14:00:39 -0400

    Update from NetSuite

---
+++ cs_invoice_attachment_validation.js
@@ -0,0 +1,55 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ClientScript
+ */
+define([], function () {
+
+    function saveRecord(context) {
+        try {
+            var rec = context.currentRecord;
+
+            // Standard Files subtab on transaction
+            var sublistId = 'mediaitem';
+            var lineCount = 0;
+            var hasFile = false;
+            var i, fileId;
+
+            try {
+                lineCount = rec.getLineCount({
+                    sublistId: sublistId
+                }) || 0;
+            } catch (e) {
+                alert('Unable to validate attached files. Please contact admin.');
+                return false;
+            }
+
+            for (i = 0; i < lineCount; i++) {
+                fileId = rec.getSublistValue({
+                    sublistId: sublistId,
+                    fieldId: 'mediaitem',
+                    line: i
+                });
+
+                if (fileId) {
+                    hasFile = true;
+                    break;
+                }
+            }
+
+            if (!hasFile) {
+                alert('Please attach at least one file before saving this Invoice.');
+                return false;
+            }
+
+            return true;
+
+        } catch (e) {
+            alert('Error while validating attached files: ' + e.message);
+            return false;
+        }
+    }
+
+    return {
+        saveRecord: saveRecord
+    };
+});
